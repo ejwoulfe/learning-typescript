bbeb90d8dafdf801b69dd191e194f4466833a6a3
Ed Woulfe <edwoulfe@gmail.com>
2021-04-20 14:11:25 -0500

    Interfaces in typescript along with class notation.

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Working");
+var UserAccount = /** @class */ (function () {
+    function UserAccount(name, id) {
+        this.name = name;
+        this.id = id;
+    }
+    return UserAccount;
+}());
+var AdminAccount = /** @class */ (function () {
+    function AdminAccount(name, id, password) {
+        this.name = name;
+        this.id = id;
+        this.password = password;
+    }
+    return AdminAccount;
+}());
+var user = new UserAccount("Frank", 2);
+var admin = new AdminAccount("Bob", 23, "secret");
+console.log(user);
+console.log(admin);
--- app.ts
+++ app.ts
@@ -1,1 +1,32 @@
-console.log("Working")
+interface User  {
+    name: string;
+    id: number;
+}
+
+class UserAccount {
+    name: string;
+    id: number;
+
+    constructor(name: string, id: number){
+        this.name = name;
+        this.id = id;
+    }
+}
+
+class AdminAccount {
+    name: string;
+    id: number;
+    password: string;
+    
+    constructor(name: string, id: number, password: string){
+        this.name = name;
+        this.id = id;
+        this.password = password;
+    }
+}
+
+const user: User = new UserAccount("Frank", 2);
+const admin :User = new AdminAccount("Bob", 23, "secret");
+
+console.log(user);
+console.log(admin);
